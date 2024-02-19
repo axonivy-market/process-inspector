07675548449b832ee85b8e3206b8f2d5bfae4180
Trung Mai <trung.maihovan@axonactive.com>
2024-02-19 09:33:00 +0700

    TE-538: Unit test findAllTasks function

--- workflow-estimator-test/src_test/com/axonivy/utils/estimator/test/WorkflowEstimatorTest.java
+++ workflow-estimator-test/src_test/com/axonivy/utils/estimator/test/WorkflowEstimatorTest.java
@@ -1,8 +1,35 @@
 package com.axonivy.utils.estimator.test;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.axonivy.utils.estimator.WorkflowEstimator;
+
+import ch.ivyteam.ivy.application.IProcessModelVersion;
+import ch.ivyteam.ivy.environment.Ivy;
 import ch.ivyteam.ivy.environment.IvyTest;
+import ch.ivyteam.ivy.workflow.IProcessStart;
+import ch.ivyteam.ivy.workflow.IWorkflowProcessModelVersion;
 
 @IvyTest
 public class WorkflowEstimatorTest {
 
+	private IProcessStart processStart;
+	private WorkflowEstimator workflowEstimator;
+	
+	private
+	
+	@BeforeEach
+	void setup() {
+		IProcessModelVersion pmv = Ivy.request().getProcessModelVersion();		
+		processStart = IWorkflowProcessModelVersion.of(pmv).findProcessStartByUserFriendlyRequestPath("MainTest/start.ivp");			
+		//workflowEstimator = new WorkflowEstimator(process, null, "");
+		
+	}
+
+	@Test
+	void shouldfindAllTasks() {
+		System.out.println("--------------------");		
+		//workflowEstimator.findAllTasks(process.);
+	}
 }
--- workflow-estimator/src/com/axonivy/utils/estimator/WorkflowEstimator.java
+++ workflow-estimator/src/com/axonivy/utils/estimator/WorkflowEstimator.java
@@ -2,25 +2,54 @@ package com.axonivy.utils.estimator;
 
 import static java.util.Collections.emptyList;
 
+import java.util.Date;
 import java.util.List;
 
 import com.axonivy.utils.estimator.model.EstimatedTask;
 
-import ch.ivyteam.ivy.bpm.engine.restricted.model.IProcess;
+import ch.ivyteam.ivy.process.model.Process;
 import ch.ivyteam.ivy.process.model.BaseElement;
+import ch.ivyteam.ivy.process.model.element.SingleTaskCreator;
 
 public class WorkflowEstimator {
-	public WorkflowEstimator(IProcess process, Enum<?> useCase, String flowName) {
-
+	
+	private Process process;	
+	private Enum<?> useCase;
+	private String flowName;
+	
+	public WorkflowEstimator(Process process, Enum<?> useCase, String flowName) {
+		this.process = process;
+		this.useCase = useCase;
+		this.flowName = flowName;
 	}
 
 	public List<EstimatedTask> findAllTasks(BaseElement startAtElement) {
-		// TODO: Implement here
-		return emptyList();
+		
+		List<BaseElement> elements = startAtElement.getRootProcess().getElements();
+		List<EstimatedTask> estimatedTasks = elements.stream()
+		//filter to get task only		
+		.filter(el -> {
+			return el instanceof SingleTaskCreator;
+		})
+		//filter the tash which have estimated if needed
+		.map(task -> createEstimatedTask((SingleTaskCreator) task))
+		.toList();
+		
+		return estimatedTasks;
 	}
 
 	public List<EstimatedTask> findTasksOnPath(BaseElement startAtElement) {
 		// TODO: Implement here
 		return emptyList();
 	}
+	
+	private EstimatedTask createEstimatedTask(SingleTaskCreator task) {
+		EstimatedTask estimatedTask = new EstimatedTask();
+		estimatedTask.setPid(task.getPid().getRawPid());
+		estimatedTask.setTaskName(task.getName());
+		estimatedTask.setEstimatedStartTimestamp(new Date());
+		
+		return estimatedTask;
+	}
+	
 }
