db37c6a08b5dd185787cf048d1eb3f87868fc5dc
Trung Mai <trung.maihovan@axonactive.com>
2024-02-29 21:01:16 +0700

    TE-538: Using recursion for both case

--- workflow-estimator/src/com/axonivy/utils/estimator/WorkflowEstimator.java
+++ workflow-estimator/src/com/axonivy/utils/estimator/WorkflowEstimator.java
@@ -41,7 +41,7 @@ public class WorkflowEstimator {
 		this.graph = new ProcessGraph(process);
 	}
 
-	public List<EstimatedTask> findAllTasks(BaseElement startAtElement) {
+	public List<EstimatedTask> findAllTasks(BaseElement startAtElement) throws Exception {
 		List<EstimatedTask> estimatedTasks = emptyList();
 
 		if (startAtElement instanceof NodeElement) {
@@ -56,19 +56,14 @@ public class WorkflowEstimator {
 		List<EstimatedTask> estimatedTasks = emptyList();
 		
 		if (startAtElement instanceof NodeElement) {
-			List<BaseElement> path = graph.findPath(startAtElement, flowName);
-			//Maybe throw an exception if there are not path
-			if(path.isEmpty()) {
-				throw new Exception("Not found");
-			}
-			
+			List<BaseElement> path = graph.findPath(startAtElement, flowName);	
 			estimatedTasks = convertToEstimatedTasks(path);
 		}
 		
 		return estimatedTasks;
 	}
 	
-	public Duration calculateEstimatedDuration(BaseElement startElement) {
+	public Duration calculateEstimatedDuration(BaseElement startElement) throws Exception {
 		List<BaseElement> path = emptyList();
 		if (startElement instanceof NodeElement && StringUtils.isNotEmpty(flowName)) {
 			path = graph.findPath(startElement, flowName);					
